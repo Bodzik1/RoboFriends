444c72dfaed4ce275fce15960135ad3194ec6ec6
Bodzik1 <bogdanmjr@gmail.com>
2023-11-06 16:31:33 -0500

    Finished Project Added

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "tachyons";
+import App from './Containers/App';
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+
+ReactDOM.render(<App /> ,document.getElementById("root"));
+reportWebVitals();
